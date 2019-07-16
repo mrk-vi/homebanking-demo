26ae5ee2e8cd1af0eb601ebfed793b4ebe918116
mirko-z <mirko.zizzari@gmail.com>
2019-07-16 15:26:12 +0200

    improved swagger and security configurations
    handling CORS requests with spring-security
    handling authentication in swagger, and export in specs

--- backend/src/main/java/uni/mirkoz/homebankingdemo/configuration/SwaggerConfig.java
+++ backend/src/main/java/uni/mirkoz/homebankingdemo/configuration/SwaggerConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -22,8 +24,34 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("uni.mirkoz.homebankingdemo.controller.ws"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(getApiInfo());
+                .apiInfo(getApiInfo())
+                .securitySchemes(Collections.singletonList(securityScheme()))
+                .securityContexts(Collections.singletonList(securityContext()));
     }
+
+    private SecurityScheme securityScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/(admin|manager|employee|customer|auth)/.*"))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(SecurityReference.builder()
+                .reference("basicAuth")
+                .scopes(authorizationScopes)
+                .build());
+    }
+
+
 
     private ApiInfo getApiInfo() {
         return new ApiInfo(
