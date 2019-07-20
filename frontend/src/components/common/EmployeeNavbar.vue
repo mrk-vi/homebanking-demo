04ddb69cca8e3dcc8f7a8a3cc65232bdb81978b4
mirko <mirko.zizzari@gmail.com>
2019-07-20 22:49:17 +0200

    list operation for employee
    and export pdf

--- frontend/src/components/common/EmployeeNavbar.vue
+++ frontend/src/components/common/EmployeeNavbar.vue
@@ -5,26 +5,32 @@
         </a>
 
         <div class="navbar-dropdown">
-            <a class="navbar-item">
-                About
-            </a>
-            <a class="navbar-item">
-                Jobs
-            </a>
-            <a class="navbar-item">
-                Contact
+            <a class="navbar-item" @click="isOperationListModalActive = true">
+                Operations List
             </a>
             <hr class="navbar-divider">
             <a class="navbar-item">
                 Report an issue
             </a>
         </div>
+
+
+        <b-modal :active.sync="isOperationListModalActive" has-modal-card>
+            <employee-operations-list></employee-operations-list>
+        </b-modal>
     </div>
 </template>
 
 <script>
+    import EmployeeOperationsList from "../employee/EmployeeOperationList";
     export default {
-        name: "EmployeeNavbar"
+        name: "EmployeeNavbar",
+        components: {EmployeeOperationsList},
+        data() {
+            return {
+                isOperationListModalActive: false
+            }
+        }
     }
 </script>
 
---
+++ frontend/src/components/employee/EmployeeOperationList.vue
@@ -0,0 +1,161 @@
+<template>
+    <div class="modal-card" style="width: auto">
+        <div class="modal-card-head">
+            <header>Operations list</header>
+        </div>
+        <section class="modal-card-body">
+            <form>
+                <b-field grouped>
+                    <b-field label="State">
+                        <b-select v-model="filter.state">
+                            <option>OPEN</option>
+                            <option>REJECTED</option>
+                            <option>AUTHORIZED</option>
+                        </b-select>
+                    </b-field>
+                    <b-field label="Type">
+                        <b-select v-model="filter.type">
+                            <option>DEPOSIT</option>
+                            <option>WITHDRAW</option>
+                            <option>TRANSFER</option>
+                            <option>REFILL</option>
+                            <option>CAR_TAX</option>
+                        </b-select>
+                    </b-field>
+                </b-field>
+                <label><strong>Date</strong></label>
+                <b-field grouped>
+                    <b-field label="From">
+                        <b-datepicker
+                                v-model="filter.fromDay"
+                                icon="calendar-range"></b-datepicker>
+                    </b-field>
+                    <b-field label="To">
+                        <b-datepicker
+                                v-model="filter.toDay"
+                                icon="calendar-range"></b-datepicker>
+                    </b-field>
+                </b-field>
+                <label><strong>Amount</strong></label>
+                <b-field grouped>
+                    <b-field label="From">
+                        <b-numberinput
+                                v-model="filter.fromAmount"
+                                controls-position="compact" icon="currency-eur"></b-numberinput>
+                    </b-field>
+                    <b-field label="To">
+                        <b-numberinput
+                                v-model="filter.toAmount"
+                                controls-position="compact" icon="currency-eur"></b-numberinput>
+                    </b-field>
+                </b-field>
+                <b-field grouped>
+                    <p class="control">
+                        <button
+                                class="button is-primary"
+                                @click="search">Search</button>
+                    </p>
+                    <p class="control">
+                        <button
+                                class="button is-success"
+                                @click="exportPDF">Export PDF</button>
+                    </p>
+                </b-field>
+            </form>
+
+            <b-table :data="operations">
+                <template slot-scope="operation">
+                    <b-table-column label="#id">
+                        {{operation.row.id}}
+                    </b-table-column>
+                    <b-table-column label="Amount">
+                        {{operation.row.amount}}
+                    </b-table-column>
+                    <b-table-column label="Iban">
+                        {{operation.row.bankAccount.iban}}
+                    </b-table-column>
+<!--                    <b-table-column label="Action">-->
+<!--                        <b-button @click="details(operation.row)">Details</b-button>-->
+<!--                    </b-table-column>-->
+                </template>
+            </b-table>
+        </section>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from "vuex";
+    import BField from "buefy/src/components/field/Field";
+    import BSelect from "buefy/src/components/select/Select";
+    import BDatepicker from "buefy/src/components/datepicker/Datepicker";
+    import BNumberinput from "buefy/src/components/numberinput/Numberinput";
+
+    export default {
+        name: "EmployeeOperationsList",
+        components: {BNumberinput, BDatepicker, BSelect, BField},
+        data() {
+            return {
+                filter: {
+                    fromAmount: null,
+                    toAmount: null,
+                    fromDay: null,
+                    toDay: null,
+                    state: null,
+                    type: null
+                },
+                operations: []
+            }
+        },
+        computed: {
+            ...mapGetters(['client'])
+        },
+        methods:{
+            parseFilter: (filter) => {
+                try {
+                    filter.toDay.setHours(filter.toDay.getHours()+24)
+
+                    const parsed = {
+                        ...filter,
+                        fromDay: filter.fromDay.toJSON(),
+                        toDay: filter.toDay.toJSON()
+                    }
+
+                    filter.toDay.setHours(filter.toDay.getHours()-24)
+                    return parsed
+                } catch (e) {
+                    return filter
+                }
+            },
+            search: async function(evt) {
+                evt.preventDefault()
+                const res = await this.client.apis['employee-dashboard'].getBankingOperationsUsingPOST({filter: this.parseFilter(this.filter)})
+                this.operations = JSON.parse(res.data)
+            },
+            exportPDF: async function(evt){
+                evt.preventDefault()
+                const res = await this.client.apis['pdf-controller'].exportBankingOperationsPDFUsingPOST({filter: this.parseFilter(this.filter)})
+
+                // HANDLING OF BLOB
+
+                const fileURL = URL.createObjectURL(res.data)
+
+                // hack that works on the most common browsers
+                var link = document.createElement('a');
+                link.href = fileURL;
+                link.download="operations.pdf";
+                link.click();
+                setTimeout(function(){
+                    // For Firefox it is necessary to delay revoking the ObjectURL
+                    window.URL.revokeObjectURL(fileURL);
+                }, 100);
+            },
+            // details: function (operation) {
+            //
+            // }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
