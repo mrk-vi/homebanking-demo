9ee37f5897f564134b43fa206361781ca64c6859
mirko <mirko.zizzari@gmail.com>
2019-07-25 22:08:00 +0200

    obtain host dynamically

--- frontend/src/service/index.js
+++ frontend/src/service/index.js
@@ -1,12 +1,14 @@
 import Swagger from 'swagger-client'
 
+export const HOST = window.location.host.split(':')[0]
+
 export const publicClient = async function () {
-    const client = await Swagger('http://localhost:8080/v2/api-docs')
+    const client = await Swagger('http://'+HOST+':8080/v2/api-docs')
     return client
 }
 
 export const basicAuthClient = async function (username, password) {
-    const client = await Swagger('http://localhost:8080/v2/api-docs', {
+    const client = await Swagger('http://'+HOST+':8080/v2/api-docs', {
         authorizations: {
             basicAuth: {username: username, password: password}
         }
